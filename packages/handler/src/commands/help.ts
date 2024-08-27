3329b627ae71972de8414ca19f281de16bc48b83
Tom <oerushia@gmail.com>
2024-08-27 07:42:30 +0100

    fix options and other stuff

---
+++ packages/handler/src/commands/help.ts
@@ -0,0 +1,15 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import type { Command } from "../services/commands.js";
+
+export const command: Command = {
+    data: new SlashCommandBuilder()
+        .setName("help")
+        .setDescription("View a list of commands from the bot")
+        .addStringOption((option) =>
+            option.setName("command").setDescription("Specific command to view").setRequired(false),
+        ),
+
+    async execute(interaction, api): Promise<void> {
+        await api.interactions.reply(interaction.id, interaction.token, { content: "Totally awesome help menu" });
+    },
+};
