3519ece1e8845938af5f268be4702e2108de292e
Tom <oerushia@gmail.com>
2024-09-05 10:45:47 +0100

    Fix pnpm check and allow docker compose to get user, pw and db from .env

--- packages/handler/src/commands/getcode.ts
+++ packages/handler/src/commands/getcode.ts
@@ -9,7 +9,7 @@ export const command: Command = {
         .setName("getcode")
         .setDescription("Grab the first result in snippet_code from a table"),
     async execute(interaction, api): Promise<void> {
-        const result = await pool.query(`SELECT snippet_code::text as code FROM bot.tbl_snippets LIMIT 1`).catch(() => {
+        const result = await pool.query("SELECT snippet_code::text as code FROM bot.tbl_snippets LIMIT 1").catch(() => {
             return { rows: [] }; // This is a bad practice and should actually be handled properly
             // Considering this is just for testing purposes, it's fine
         });
