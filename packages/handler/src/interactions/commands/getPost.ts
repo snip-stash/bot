168a11fa8351b20cf8dcef36f67f2ce258c060ed
Tom <oerushia@gmail.com>
2024-09-15 14:20:43 +0100

    fix database integration

--- packages/handler/src/interactions/commands/getPost.ts
+++ packages/handler/src/interactions/commands/getPost.ts
@@ -5,6 +5,8 @@ import {
     SlashCommandBuilder,
     bold,
     codeBlock,
+    formatEmoji,
+    inlineCode,
 } from "@discordjs/builders";
 import { ButtonStyle } from "@discordjs/core";
 import prisma from "database";
@@ -16,9 +18,9 @@ export const interaction: Command = {
     data: new SlashCommandBuilder()
         .setName("get-post")
         .setDescription("Grab a post that you can review.")
-        .addNumberOption((option) => option.setName("id").setDescription("The ID of the post").setRequired(true)),
+        .addStringOption((option) => option.setName("id").setDescription("The ID of the post").setRequired(true)),
     async execute(interaction): Promise<void> {
-        const getOption = getCommandOption("id", ApplicationCommandOptionType.Number, interaction.options) || -1;
+        const getOption = getCommandOption("id", ApplicationCommandOptionType.String, interaction.options) || "";
         const getQuery = await (await prisma).post.findUnique({
             include: { paste: true },
             where: { id: getOption },
@@ -33,17 +35,23 @@ export const interaction: Command = {
             .setStyle(ButtonStyle.Danger);
 
         const embed = new EmbedBuilder()
-            .setTitle(getQuery?.title)
-            .setDescription(
-                `   ${bold("Content")}
-                    ${getQuery?.description}\n${codeBlock("js", getQuery?.paste?.content || "No Content")}
-                    ${getQuery?.paste?.content ? `${bold("Error")}\n${codeBlock("js", getQuery?.paste?.content)}` : ""}\n
-                    ${bold("Likes")} : 0 ~ ${bold("Dislikes")} : 0
-                `,
-            )
+            .setDescription(`
+                ${formatEmoji("1283395868648673331")} ${bold(inlineCode(getQuery.title))}
+                ${formatEmoji("1283395921366880266")} ${bold(inlineCode("description"))}
+                ${codeBlock(getQuery.language, getQuery.description)}
+                ${formatEmoji("1283395929444978740")} ${bold(inlineCode("Code"))}
+                ${codeBlock(getQuery.language, getQuery.paste[0]?.content || "")}
+                ${
+                    getQuery.paste[1]?.content_type === "ERROR"
+                        ? `${formatEmoji("1283395936550260818")} ${bold(inlineCode("Error"))}
+                ${codeBlock(getQuery.language, getQuery.paste[1].content)}`
+                        : ""
+                }
+                ${formatEmoji("1283395907382939730")} ${bold(inlineCode("likes"))}: ${getQuery.likes} ~ ${formatEmoji("1283395887397343263")} ${bold(inlineCode("dislikes"))}: ${getQuery.dislikes}
+                `)
             .setColor(0x2f3136)
             .setFooter({
-                text: `• View the comments at https://snipstash.com/post/${getQuery?.id}`,
+                text: `• View the comments at https://snipstash.com/post/${getQuery.id}`,
             });
 
         const row = new ActionRowBuilder<ButtonBuilder>().addComponents(likeButton, dislikeButton);
--- packages/handler/src/interactions/modals/grabPostData.ts
+++ packages/handler/src/interactions/modals/grabPostData.ts
@@ -2,6 +2,54 @@ import { EmbedBuilder, bold, codeBlock, formatEmoji, inlineCode } from "@discord
 import prisma from "database";
 import type { Modal } from "../../services/commands.js";
 
+// Replace this function someplace else, like in a utility file.
+async function createPost(
+    interaction: any,
+    code: string,
+    error: string | null,
+    title: string,
+    description: string,
+    language: string,
+) {
+    // There needs to be a check to see if the user already exists in the database.
+    // Add this whenever this function gets replaced
+    const user = await (await prisma).user.create({
+        data: {
+            id: interaction.member_id,
+            discord_id: interaction.member_id,
+            username: interaction.member_name,
+            premium: false,
+            code_runs_left: 30,
+        },
+    });
+    
+    const post = await (await prisma).post.create({
+        data: {
+            paste: {
+                create: [
+                    {
+                        content: code,
+                        content_type: "CODE",
+                    },
+                    {
+                        content: error || "",
+                        content_type: "ERROR",
+                    },
+                ],
+            },
+            uploader_id: user.id,
+            title: title,
+            description: description,
+            likes: 0,
+            dislikes: 0,
+            language: language,
+        },
+        include: { uploader: true, paste: true },
+    });
+
+    return post;
+}
+
 export const interaction: Modal = {
     custom_id: "create-post-modal",
     async execute(interaction): Promise<void> {
@@ -19,34 +67,13 @@ export const interaction: Modal = {
             return;
         }
 
-        const createPost = await (await prisma).post.create({
-            data: {
-                uploader: {
-                    create: {
-                        discord_id: interaction.member_id,
-                        username: interaction.member_name,
-                        premium: false,
-                        code_runs_left: 30,
-                    },
-                },
-                paste: {
-                    create: {
-                        content: code,
-                        content_type: "CODE",
-                        uploader_id: interaction.member_id,
-                    },
-                },
-                title: title,
-                description: description,
-            },
-            include: { uploader: true, paste: true },
-        });
+        const prismaPost = await createPost(interaction, code, error, title, description, language);
 
         const embed = new EmbedBuilder()
             .setDescription(`
                 ${formatEmoji("1283395868648673331")} ${bold(inlineCode(title))}
                 ${formatEmoji("1283395921366880266")} ${bold(inlineCode("description"))}
-                ${codeBlock(description)}
+                ${codeBlock(language, description)}
                 ${formatEmoji("1283395929444978740")} ${bold(inlineCode("Code"))}
                 ${codeBlock(language, code)}
                 ${
@@ -57,7 +84,7 @@ export const interaction: Modal = {
                 }
                 `)
             .setColor(0x2f3136)
-            .setFooter({ text: `ID: ${createPost.id}` });
+            .setFooter({ text: `ID: ${prismaPost.id}` });
         await interaction.reply({
             content: bold("You have successfully made a post!"),
             embeds: [embed],
